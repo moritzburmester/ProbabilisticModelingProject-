5c8e4b2391d06ccdb0664bbd9a8d296ffff4625b
FLX-20 <70955540+FLX-20@users.noreply.github.com>
2024-06-25 14:03:22 +0200

    Add files via upload

---
+++ edl_net_felix/main.py
@@ -0,0 +1,46 @@
+import torch
+import torch.optim as optim
+from edl_net import EDLNet, model_training, evaluate_model
+from auxiliary_functions import plot_dirichlet_parameters, get_data_loaders, plot_first_images_of_each_class, image_size_channels
+
+
+def main():
+    # configuration parameters
+    num_epochs = 30
+    num_classes = 10
+    dataset_name = 'MNIST'
+    save_path = './edl_mnist_35.pth'
+
+    # loading the data
+    train_loader, test_loader = get_data_loaders(dataset_name, batch_size=200, num_workers=0, root='./data',
+                                                 selected_classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    # plot the first image of each class
+    plot_first_images_of_each_class(train_loader, num_classes, dataset_name)
+
+    # get image size and channels
+    input_size, input_channels = image_size_channels(train_loader)
+
+    model = EDLNet(input_channels=input_channels, num_classes=num_classes, dropout=False, input_size=input_size)
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=0.005)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+
+    model_training(
+        model,
+        train_loader,
+        test_loader,
+        num_classes,
+        optimizer,
+        num_epochs=num_epochs,
+        save_path=save_path,
+        visualize_dir=False,
+    )
+
+    #evaluate_model(model, './two_test_img.png', num_classes=3, input_channels=1, input_size=28,test_loader=test_loader)
+    evaluate_model(save_path,test_loader)
+
+if __name__ == "__main__":
+    main()
