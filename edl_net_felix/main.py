a1f30580e0ad3645a41806f03044ff70a079e88c
FLX-20 <70955540+FLX-20@users.noreply.github.com>
2024-06-30 10:31:40 +0200

    Update main.py

--- edl_net_felix/main.py
+++ edl_net_felix/main.py
@@ -3,20 +3,20 @@ import torch.optim as optim
 from edl_net import EDLNet, model_training
 from auxiliary_functions import (plot_dirichlet_parameters, get_data_loaders, classify_uploaded_image,
                                  plot_first_images_of_each_class, image_size_channels, evaluate, test_single_image,
-                                 rotating_image_classification)
+                                 rotating_image_classification, noisy_image_classification)
 
 def main():
     # Configuration parameters
-    selected_classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # Select classes
-    num_epochs = 25
+    selected_classes = [0, 1, 2,3, 4,5,6,7,8,9]  # Select classes
+    num_epochs = 100  # Number of epochs
     num_classes = len(selected_classes)  # Dynamically set the number of classes based on selected_classes
-    dataset_name = 'MNIST'  # Focus on MNIST
-    save_path = './edl_mnist.pth'
-    image_path = './data/yoda.jpg'  # Path to the uploaded image
-    img_path = './data/yoda.jpg'  # Path to the uploaded image 2nd function
+    dataset_name = 'FashionMNIST'  # Focus on MNIST
+    save_path = './edl_fashion_mnist.pth'
+    image_path = './one.jpg'  # Path to the uploaded image
+    img_path = './one.jpg'  # Path to the uploaded image 2nd function
 
     # Loading the data
-    test_dataset, train_loader, test_loader = get_data_loaders(dataset_name, batch_size=1000, num_workers=0, root='./data',
+    test_dataset, train_loader, test_loader = get_data_loaders(dataset_name, batch_size=1000, num_workers=2, root='./data',
                                                  selected_classes=selected_classes)
 
     # Plot the first image of each class
@@ -32,21 +32,24 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
 
-    model = model_training(
-        model,
-        train_loader,
-        test_loader,
-        num_classes=num_classes,
-        selected_classes=selected_classes,
-        optimizer=optimizer,
-        num_epochs=num_epochs,
-        save_path=save_path,
-        visualize_dir=False,
-    )
+    # model = model_training(
+    #     model,
+    #     train_loader,
+    #     test_loader,
+    #     num_classes=num_classes,
+    #     selected_classes=selected_classes,
+    #     optimizer=optimizer,
+    #     num_epochs=num_epochs,
+    #     save_path=save_path,
+    #     visualize_dir=False,
+    # )
 
 
     # Evaluate the model using different eval functions
     print("Final Evaluation:")
+
+    # loaf the model
+    model.load_state_dict(torch.load(save_path))
     evaluate(model, test_loader, num_classes, selected_classes)
 
     # Classify the uploaded image
@@ -61,10 +64,13 @@ def main():
     digit_one_images = [(idx, data) for idx, data in enumerate(test_dataset) if data[1] == 1]
     for idx, (image_idx, (digit_one, _)) in enumerate(digit_one_images):
         print(f'Using image at index {image_idx} from test_dataset for visualization.')
+        noisy_image_classification(
+            model, img=digit_one, threshold=0.3, num_classes=num_classes,
+            selected_classes=selected_classes, plot_dir='noise_classification', file_name=f'noisy_image_{idx}')
         rotating_image_classification(
-            model, img=digit_one, threshold=0.5, num_classes=num_classes,
+            model, img=digit_one, threshold=0.3, num_classes=num_classes,
             selected_classes=selected_classes, plot_dir='rotation_classification', file_name=f'rotating_image_{idx}')
-        if idx >= 1:  # Stop after processing 3 images
+        if idx >= 2:  # Stop after processing 3 images
             break
 
 if __name__ == "__main__":
